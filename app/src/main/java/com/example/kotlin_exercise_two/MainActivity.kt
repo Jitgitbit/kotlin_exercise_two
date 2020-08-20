28966a15d1aa253e1cb835a7c29836299f52decb
Jitgitbit <rowowolf@gmail.com>
2020-08-20 15:32:43 +0200

    playing with activities

--- app/src/main/java/com/example/kotlin_exercise_two/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_exercise_two/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.kotlin_exercise_two
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +8,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        window.decorView.setBackgroundColor(Color.RED)
     }
 }
