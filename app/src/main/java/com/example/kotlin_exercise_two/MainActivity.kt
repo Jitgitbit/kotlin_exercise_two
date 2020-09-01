4782af7433b4362d667761c5848372ea9a3411c6
Jitgitbit <rowowolf@gmail.com>
2020-09-01 12:09:26 +0200

    layout and kotlin coming together

--- app/src/main/java/com/example/kotlin_exercise_two/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_exercise_two/MainActivity.kt
@@ -3,10 +3,16 @@ package com.example.kotlin_exercise_two
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        infoTextView.visibility = View.INVISIBLE
+
+        calculateButton.setOnClickListener {  }
     }
 }
